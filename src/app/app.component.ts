61822efde30f32b16c62de97c0d861f68772d13c
Josue ortiz <ortizjd0036@gmail.com>
2024-01-11 18:43:16 -0400

    first commit angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import {HeroesComponent } from './heroes/heroes.component'
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet , HeroesComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Marvel Heroes';
+}
