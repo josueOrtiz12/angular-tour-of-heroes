44c1e0c2058df2a7436e5fea5fcd7a6667cf487b
Josue ortiz <ortizjd0036@gmail.com>
2024-01-15 11:09:35 -0400

    create service for get heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -3,8 +3,8 @@ import { Hero } from '../interface/hero'
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { NgFor } from '@angular/common';
-import { HEROES } from '../mock-heroes';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component'
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -18,10 +18,24 @@ import { HeroDetailComponent } from '../hero-detail/hero-detail.component'
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
-  heroes = HEROES;
+  heroes: Hero[] = []
   
   selectedHero? : Hero;
   onSelect(hero : Hero):void{
     this.selectedHero = hero;
+  }
+  
+
+  constructor(private heroeService : HeroService){
+
+  }
+
+  getHeroes() : void {
+    this.heroeService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit()  :void {
+    this.getHeroes();
   }
 }
